f3e0c2c102b5293b0cb4d614bac21ea6d4445fab
Muhammad Arsalan Khan <arsal4an@gmail.com>
2023-11-28 14:43:27 +0500

    Added Datanodes.py and parse_forward.py empty files

---
+++ dataNodes.py
@@ -0,0 +1,11 @@
+'''
+    DataNodes.py is a python file containing the structure definition of each data 
+    element being used in our search engine.
+
+'''
+
+class DocElement():
+    break
+
+class WordElement():
+    break
