fc30404e4677ec594d09bd734d11737ae1155822
Muhammad Arsalan Khan <arsal4an@gmail.com>
2023-11-29 14:10:17 +0500

    Added parse_forward comments

---
+++ parse_forward.py
@@ -0,0 +1,17 @@
+'''
+    The following document contains the set of functions being used to create the forward index.
+
+    In the forward indexing algorithm, a forward index will be created of the form:
+
+    DOC_ELEMENT: WORD_ELEMENT, WORD_ELEMENT, WORD_ELEMENT
+    DOC_ELEMENT: WORD_ELEMENT, WORD_ELEMENT, WORD_ELEMENT
+    DOC_ELEMENT: WORD_ELEMENT, WORD_ELEMENT, WORD_ELEMENT
+
+    Each DOC_ELEMENT and WORD_ELEMENT is defined in the dataNodes.py file
+
+    We can use protocol buffers to store the data structure from this file
+
+    We will divide each 1000 articles for 
+
+
+'''
