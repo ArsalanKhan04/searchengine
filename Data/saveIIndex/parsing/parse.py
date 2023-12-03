3e3a22236b788abfe5bbabacc968cefe1c4835eb
imama13 <imamahamid2006@gmail.com>
2023-12-03 20:39:59 +0500

    parsing function for inverted index

--- Data/saveIIndex/parsing/parse.py
+++ Data/saveIIndex/parsing/parse.py
@@ -16,12 +16,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.getcwd()))))
 from Data.saveFIndex import forwardindex_file_pb2 as fpb
 import Data.saveIIndex.invDS as invDS
 from wordlexicon import lexicon_list
+from Data.saveIIndex.helper.helperfuncs import return_docelem
 
-def parse(forward_index):
+
+def parse(forward_index, invertedindex):
     # Modify this code accordingly
-    invertedindex = invDS.InvertedIndex(len(lexicon_list))
     for eachdoc in forward_index.docelement:
-        for eachwordelem in eachdoc:
-            
+        for eachwordelem in eachdoc.wordelement:
+            docelem = return_docelem(eachdoc.doc_id, eachwordelem)
+            invertedindex.bin_insert(docelem, eachwordelem.word_id)
     return invertedindex
 
