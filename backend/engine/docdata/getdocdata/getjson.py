d9cdc41be25c8a11b6e3da4617d8202b5ba8c4cc
Muhammad Arsalan Khan <arsal4an@gmail.com>
2023-12-29 05:42:00 +0500

    in working condition right now without style

--- backend/engine/docdata/getdocdata/getjson.py
+++ backend/engine/docdata/getdocdata/getjson.py
@@ -22,15 +22,23 @@ def get_docdata(doc_id):
     
     else:
         docdata = open_docs[docdata_index]
-
-    eachdocdata = docdata.eachdoc[doc_id%forwardindexlimit]
-    return {
-        "url":eachdocdata.url,
-        "title":eachdocdata.title,
-        "date":eachdocdata.date,
-        "chars500":eachdocdata.chars500,
-        "author":eachdocdata.author
-    }
+    try:
+        eachdocdata = docdata.eachdoc[doc_id%forwardindexlimit]
+        return {
+            "url":eachdocdata.url,
+            "title":eachdocdata.title,
+            "date":eachdocdata.date,
+            "chars500":eachdocdata.chars500,
+            "author":eachdocdata.author
+        }
+    except Exception:
+        return {
+            "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "title":"This page is lost",
+            "date":"I don't know date",
+            "chars500":"This is a kind of easter egg i guess, this page is just not present in the docdata",
+            "author":"Arsalan"
+        }
 
 def flush():
     global open_docs
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function App() {
+
+const SearchApp = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [time, setTime] = useState(0.0);
   const [count, setCount] = useState(0.0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const resultsPerPage = 10; // Set the number of results to display per page
 
   const handleSearch = () => {
     axios
@@ -14,12 +17,35 @@ function App() {
         setResults(response.data.result);
         setTime(response.data.time);
         setCount(response.data.count);
+        setCurrentPage(1); // Reset current page after search
       })
       .catch((error) => {
         console.error('Error fetching search results:', error);
       });
   };
 
+  const renderResults = () => {
+    const startIndex = (currentPage - 1) * resultsPerPage;
+    const endIndex = startIndex + resultsPerPage;
+    const currentResults = results.slice(startIndex, endIndex);
+    
+    return (
+      <ul>
+        {currentResults.map((result, index) => (
+          <li key={index}>
+            <a href={result.url}>{result.title}</a>
+            <p>Date: {result.date}</p>
+            <p>{result.chars500}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  const totalPages = Math.ceil(results.length / resultsPerPage);
   return (
     <div>
       <h1>Search App</h1>
@@ -33,27 +59,31 @@ function App() {
         <button onClick={handleSearch}>Search</button>
       </div>
       <div>
-        {
-          count !== 0 ? (
-            <h3>Generated {count.toFixed(2)} pages in {time} s</h3>
-          ) : (
-            <h3>Enter Query Above</h3>
-          )
-        }
-        
+        {/* Display search statistics */}
+        {count !== 0 ? (
+          <h3>Generated {count.toFixed(2)} pages in {time} s</h3>
+        ) : (
+          <h3>Enter Query Above</h3>
+        )}
+
         <h2>Search Results:</h2>
-        <ul>
-          {results.map((result, index) => (
-            <li key={index}>
-              <a href={result.url}>{result.title}</a>
-              <p>Date: {result.date}</p>
-              <p>{result.chars500}</p>
-            </li>
-          ))}
-        </ul>
+
+        {/* Render current page results */}
+        {renderResults()}
+
+        {/* Pagination controls */}
+        <div>
+          <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+            Previous
+          </button>
+          <span> Page {currentPage} of {totalPages} </span>
+          <button disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default SearchApp;
