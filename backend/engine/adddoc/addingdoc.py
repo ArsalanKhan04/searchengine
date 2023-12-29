419eb5822b39292fa692961b7d2977df7c3a254f
Muhammad Arsalan Khan <arsal4an@gmail.com>
2023-12-29 06:01:23 +0500

    Final Commit

---
+++ backend/engine/adddoc/addingdoc.py
@@ -0,0 +1,12 @@
+'''
+
+    This file will contain code where a single document will be parsed and made into an inverted index
+
+'''
+
+# Because we will be getting the data in the same dictionary format as previously,
+# we will use it accordingly
+
+from forwardindex.parse_forward import parse
+
+
